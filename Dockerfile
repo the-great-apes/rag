319cd80bff1f21b493710e598d89993113b18adf
farkas93 <zsombor.kalotay@gmx.ch>
2024-06-30 11:02:51 +0200

    restructure

--- Dockerfile
+++ Dockerfile
@@ -9,5 +9,6 @@ RUN apt  update && apt install -y git
 RUN pip install -U -r requirements.txt
 # pull data
 RUN dvc update -R data/
+RUN dvc repro
 
-CMD [ "dvc", "repro"]
+CMD [ "python", -m "src.upload_to_mongodb"]
--- src/upload_to_mongodb.py
+++ src/upload_to_mongodb.py
@@ -1,3 +1,8 @@
+import json
+import logging
+import os
+from collections import defaultdict
+from helpers import Summary, list_json_files
 from pymongo import MongoClient
 from pymongo.errors import PyMongoError
 import logging
@@ -44,3 +49,54 @@ def write_document(ref_key, data):
     finally:
         if 'client' in locals():
             client.close()  # Always close the client connection at the end
+
+
+try:
+    log_level = os.environ['LOG_LEVEL'].upper()
+except KeyError:
+    log_level = 'INFO'
+
+log_level_mapping = {
+    'DEBUG': logging.DEBUG,
+    'INFO': logging.INFO,
+    'WARNING': logging.WARNING,
+    'ERROR': logging.ERROR,
+    'CRITICAL': logging.CRITICAL
+}
+
+logging.basicConfig(level=log_level_mapping.get(log_level, logging.WARNING))
+
+DATA_PATH = "/app/data"
+# Print the parsed data
+
+COMPANY_REFS = ["ABB", "IBM", "PostFinance", "Raiffeisen", "Siemens", "UBS"]
+
+
+def write_companies():
+    write_document("companies", {"companies": list(data.keys())})
+
+def write_years(comp: str):
+    write_document(f"{comp}-years", {"years": list(data[comp].keys())})
+
+def write_report(comp: str, year: str):
+    write_document(f"{comp}-{year}", {"report": data[comp][int(year)].model_dump_json()})
+
+
+def main():
+    data = defaultdict(defaultdict)
+
+    json_files = list_json_files(Path("data/summary"))
+
+    for s in [Summary.load(f) for f in json_files]:
+        data[s.company][s.year] = s
+
+    write_companies()
+    for c in data.keys():
+        write_years(c)
+        for y in data['c'].keys():
+            write_report(c, y)
+    
+
+        
+if __name__ == "__main__":
+    main()
